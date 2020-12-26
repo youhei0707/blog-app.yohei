bee2b193d702afb4444b458002c6160da4df018c
Youhei07 <youxicun.07@gmail.com>
2020-12-26 21:37:09 +0900

    本番環境ではdotenvを読み込まないように変更

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,7 @@ module BlogApp
       Dotenv::Railtie.load
     end
 
+    
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
